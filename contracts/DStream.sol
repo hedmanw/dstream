ca47f9e2d17ca563562191dd978ad2f144fa6d31
Joel Torstensson <oed3933@gmail.com>
2015-05-16 08:43:11 +0200

    Created contract.

---
+++ contracts/DStream.sol
@@ -0,0 +1,27 @@
+contract DStream {
+
+    address owner;
+    mapping public (address => uint) redeemable;
+    uint public depositTotal;
+    uint public depositLeft;
+    uint public filesize;
+
+    function DStream(uint size) {
+        owner = msg.sender;
+        depositTotal = msg.value;
+        filesize = size;
+    }
+
+    function confirm(address provider, uint dataAmount) {
+        if (owner == msg.sender) {
+            redeemable[provider] += depositTotal*(dataAmount/filesize);
+        }
+    }
+
+    function redeem() {
+        if (redeemable[msg.sender] > 0) {
+            msg.sender.send(redeemable[msg.sender]);
+            redeemable[msg.sender] = 0;
+        }
+    }
+}
